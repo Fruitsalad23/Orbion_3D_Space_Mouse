d0bedb7bc1b48874cfb6f059ae4495a74dd12f8b
Aras Balali Moghaddam <arasbm@gmail.com>
2023-03-01 14:48:04 -0800

    modified firmware
    Add firmware folder and platformio files
    This version of the firmware has 7 custom keys

---
+++ firmware/src/Settings.h
@@ -0,0 +1,20 @@
+//**************************************************************************************************/
+//                                          Pin Defines                                         */
+//**************************************************************************************************/
+
+
+#define LED               15         // Nopixel
+#define NUMPIXELS         7          // Number of led on the strip (default 7)
+#define DT                5          // DT Encoder                      
+#define CLK               4          // CLK Encoder        
+#define horzPin           A2         // VRY Joystick
+#define vertPin           A3         // VRX Joystick
+#define joyButt           A1         // SW Joystick
+#define button1           7          // Rear Button
+#define button2           8          // Rear Button    
+#define button3           9          // Rear Button   
+#define button4           18         // Custom Button4
+#define button5           14         // Custom Button5 
+#define button6           16         // Custom Button6 
+#define button7           10         // Custom Button7  
+#define timeout           60000      // time to turn off the display and led (60 seconds * 1000)
